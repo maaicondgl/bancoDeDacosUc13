4c4e408f50d2592b1394ef7b796249a70637224c
maaicondgl <maaicondgl@outlook.com>
2022-05-15 11:16:26 -0300

    atividades e encontros

---
+++ encontro2.sql
@@ -0,0 +1,9 @@
+INSERT INTO  Usuarios VALUES ('donodopeugeot@email.com', '2022')
+INSERT INTO Classes VALUES ('Monge', 'Descricao')
+INSERT INTO Habilidades VALUES ('Utani hur', 'mareaTurbo')
+INSERT INTO Persoangens (NomePersonagem, UsuarioId, ClasseId) VALUES ('PeugeotSemFreio', 2, 2)
+INSERT INTO ClasseHabilidade ( ClasseId, habilidadeId) VALUES (2, 2), (2, 3)
+
+SELECT * FROM  Usuarios
+SELECT * FROM  Persoangens
+SELECT * FROM  Habilidades
